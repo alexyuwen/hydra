1f5f8dc03ecda1e744b695157ec50630b6485ef8
Alex Yuwen <alexyuwen@gmail.com>
2025-09-22 15:55:51 -0700

    .

--- sketches/still/double gradient.js
+++ sketches/still/double gradient.js
@@ -1,3 +1,4 @@
+let layer1 = gradient().g().repeatY(2).add(shape(1, 0, 0))
 let layer2 = gradient().g().invert().repeatY(2).mask(shape(1, 0, 0))
 
-gradient().g().repeatY(2).add(shape(1, 0, 0)).layer(layer2).out()
+layer1.layer(layer2).out()
